1fc12314ea8925b3d30e731da6fb94696a257b9e
Alishba Shafiq <142573703+Alishba-12@users.noreply.github.com>
2026-03-26 10:51:55 +0500

    Update HoenScannerApplication.java

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -1,28 +1,47 @@
-package com.skyscanner;
+package com.skyscanner.hoenScanner;
 
-import io.dropwizard.core.Application;
-import io.dropwizard.core.setup.Bootstrap;
-import io.dropwizard.core.setup.Environment;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.skyscanner.hoenScanner.api.SearchResult;
+import com.skyscanner.hoenScanner.resources.SearchResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
-
-    public static void main(final String[] args) throws Exception {
+    
+    private List<SearchResult> searchResults = new ArrayList<>();
+    
+    public static void main(String[] args) throws Exception {
         new HoenScannerApplication().run(args);
     }
-
+    
     @Override
-    public String getName() {
-        return "hoen-scanner";
+    public void initialize(Bootstrap<HoenScannerConfiguration> bootstrap) {
+        // Nothing needed here
     }
-
+    
     @Override
-    public void initialize(final Bootstrap<HoenScannerConfiguration> bootstrap) {
-
+    public void run(HoenScannerConfiguration configuration, Environment environment) throws Exception {
+        // Load rental cars
+        ObjectMapper mapper = new ObjectMapper();
+        InputStream rentalCarsStream = getClass().getClassLoader().getResourceAsStream("rental_cars.json");
+        if (rentalCarsStream != null) {
+            List<SearchResult> rentalCars = mapper.readValue(rentalCarsStream, new TypeReference<List<SearchResult>>() {});
+            searchResults.addAll(rentalCars);
+        }
+        
+        // Load hotels
+        InputStream hotelsStream = getClass().getClassLoader().getResourceAsStream("hotels.json");
+        if (hotelsStream != null) {
+            List<SearchResult> hotels = mapper.readValue(hotelsStream, new TypeReference<List<SearchResult>>() {});
+            searchResults.addAll(hotels);
+        }
+        
+        // Register the resource with the search results
+        environment.jersey().register(new SearchResource(searchResults));
     }
-
-    @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
-
-    }
-
 }
